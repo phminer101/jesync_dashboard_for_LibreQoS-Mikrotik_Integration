30f2d14f5ad30f4e2615928e2716d3d463bc0b77
jesync <jesienazarethnario@gmail.com>
2025-03-30 07:14:42 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ FILES = {
     "config.json": "/opt/libreqos/src/config.json",
     "updatecsv.py": "/opt/libreqos/src/updatecsv.py",
     "jesyncmt.json": "/opt/libreqos/src/jesync_dashboard/jesyncmt.json",  # ✅ Add this line
-    "network.json": "/etc/libreqos/network.json",
+    "network.json": "/etc/libreqos/src/network.json",
     "ShapedDevices.csv": "/etc/libreqos/ShapedDevices.csv",
     "lqos.conf": "/etc/libreqos/lqos.conf"
 }
