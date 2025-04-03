e9692cd47f7d2f5d6aa414f28937e77d8b0ac0b8
jesync <jesienazarethnario@gmail.com>
2025-04-04 00:05:19 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+import json
+import ast
+
+FILES = {
+    "jesync_static_device.json": "/opt/libreqos/src/jesync_static_device.json",
+    "config.json": "/opt/libreqos/src/config.json",
+    "updatecsv.py": "/opt/libreqos/src/updatecsv.py",
+    "jesyncmt.json": "/opt/libreqos/src/jesync_dashboard/jesyncmt.json",
+    "network.json": "/opt/libreqos/src/network.json",
+    "ShapedDevices.csv": "/opt/libreqos/src/ShapedDevices.csv",
+    "lqos.conf": "/etc/lqos.conf"
+}
+
+
+
+
+VIEW_ONLY = ["ShapedDevices.csv"]  # mark which files are read-only
+
+def validate_and_save(filename, content):
+    try:
+        if filename.endswith(".json"):
+            json.loads(content)
+        elif filename.endswith(".py"):
+            ast.parse(content)
+        with open(FILES[filename], "w") as f:
+            f.write(content)
+        return True, "Saved successfully"
+    except Exception as e:
+        return False, str(e)
